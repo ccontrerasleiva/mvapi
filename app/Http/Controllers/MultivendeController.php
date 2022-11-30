d80cdac5dd8cb229541526144a5076cfe8666f9e
Cristian Contreras <ccontrerasl@gmail.com>
2022-11-30 17:43:13 +0000

    agrega configuracion nginx

--- app/Http/Controllers/MultivendeController.php
+++ app/Http/Controllers/MultivendeController.php
@@ -30,7 +30,7 @@ class MultivendeController extends Controller
         $headers[] = 'Authorization: Bearer '.$this->token;
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-        $result = curl_exec($ch);
+        $result = curl_exec($ch);   
         if (curl_errno($ch)) {
             echo 'Error:' . curl_error($ch);
         }
@@ -39,5 +39,26 @@ class MultivendeController extends Controller
         //return response($r->path());*/
     }
 
-    
+    public function postRequest(Request $r){
+        $input = $r->all();
+        //$params = http_build_query($input);
+        $url = $this->url.$r->path();
+        //echo $url;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+
+        $headers = array();
+        $headers[] = 'Authorization: Bearer '.$this->token;
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $result = curl_exec($ch);   
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+        }
+        curl_close($ch);
+        return response($result)->header('Content-Type','application/json');
+        //return response($r->path());*/
+    }
 }
